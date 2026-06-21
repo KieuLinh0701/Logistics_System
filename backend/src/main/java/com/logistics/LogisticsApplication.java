bd4f112cd67f71da8b12f357f23bb8d24ecfef60
KieuLinh0701 <uynukieulinh@gmail.com>
2026-06-21 23:07:19 +0700

    fix: send email on the prod environment

--- backend/src/main/java/com/logistics/LogisticsApplication.java
+++ backend/src/main/java/com/logistics/LogisticsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties(JwtConfig.class)
 @EnableScheduling
 @EnableCaching
+@EnableAsync
 public class LogisticsApplication {
 
     public static void main(String[] args) {
