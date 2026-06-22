b10d035188aa24289f4c5e71cc2ab22797e559ff
KieuLinh0701 <uynukieulinh@gmail.com>
2026-06-22 22:35:48 +0700

    feat: minor error

--- frontend/src/pages/user/employee/audit-logs-user/components/Actions.tsx
+++ frontend/src/pages/user/employee/audit-logs-user/components/Actions.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Button, Space} from 'antd';
 import {FileExcelOutlined} from '@ant-design/icons';
+import {hasPermissionGroup} from "../../../../../utils/authUtils.ts";
 
 interface ActionsProps {
     onExport: () => void;
@@ -13,14 +14,16 @@ const Actions: React.FC<ActionsProps> = ({
                                          }) => {
     return (
         <Space align="center">
-            <Button
-                className="success-button"
-                icon={<FileExcelOutlined/>}
-                onClick={onExport}
-                disabled={total === 0}
-            >
-                Xuất Excel
-            </Button>
+            {hasPermissionGroup(['GROUP_USER', 'USER_AUDIT_LOG_DETAIL_EXPORT']) && (
+                <Button
+                    className="success-button"
+                    icon={<FileExcelOutlined/>}
+                    onClick={onExport}
+                    disabled={total === 0}
+                >
+                    Xuất Excel
+                </Button>
+            )}
         </Space>
     );
 };
