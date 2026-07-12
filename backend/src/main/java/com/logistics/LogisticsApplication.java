a092fef1738aaf4dba041b6fadf416338de421c1
KieuLinh0701 <uynukieulinh@gmail.com>
2026-07-13 04:25:25 +0700

    fix: time zone

--- backend/src/main/java/com/logistics/LogisticsApplication.java
+++ backend/src/main/java/com/logistics/LogisticsApplication.java
@@ -1,6 +1,7 @@
 package com.logistics;
 
 import com.logistics.config.JwtConfig;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,6 +11,8 @@ import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties(JwtConfig.class)
@@ -18,6 +21,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableRetry
 public class LogisticsApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+    }
 
     public static void main(String[] args) {
 
